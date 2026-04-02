3a5b54cbbd9c1d078c1333b8a9676027c1c3ff77
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-04-02 15:11:31 +0300

    New sample project - initial scan

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -4,4 +4,4 @@ export default {
   organization: "volodymyr-kulyk-1-levelaccess-com-clkuq",
   project: "level-ci-sample-github-1",
   token: process.env.LEVEL_CI_TOKEN,
-} satisfies Config;
+} satisfies Config; 
